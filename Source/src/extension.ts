503bd49691b4f094b7a4b89bc0a24f28674242a2
filmee24 <filmee24@gmail.com>
2022-06-24 12:07:44 +0200

    Cleanup

--- Source/src/extension.ts
+++ Source/src/extension.ts
@@ -1,61 +1,45 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
+'use strict';
 
 import * as path from 'path';
-import { ExtensionContext } from 'vscode';
+import { workspace, ExtensionContext } from 'vscode';
 
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
-} from 'vscode-languageclient';
-
-let client: LanguageClient;
-
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind
+} from 'vscode-languageclient/node';
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	const serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
+    // The server is implemented in node
+    let serverExe = 'dotnet';
 
-	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'html1' }]
-	};
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    let serverOptions: ServerOptions = {
+        run: { command: serverExe, args: ['C:\Users\chris\Documents\Projekte\Backlang-VSC\LSP\bin\Debug\net7.0\LSP-Server.dll'] },
+        debug: { command: serverExe, args: ['C:\Users\chris\Documents\Projekte\Backlang-VSC\LSP\bin\Debug\net7.0\LSP-Server.dll'] }
+    }
 
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
-		serverOptions,
-		clientOptions
-	);
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [
+            {
+                pattern: '**/*.back',
+            }
+        ],
+        synchronize: {
+            // Synchronize the setting section 'languageServerExample' to the server
+            configurationSection: 'languageServerExample',
+            fileEvents: workspace.createFileSystemWatcher('**/*.back')
+        },
+    }
 
-	// Start the client. This will also launch the server
-	client.start();
-}
+    // Create the language client and start the client.
+    const client = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions);
+    
+    let disposable = client.start();
 
-export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+
 }
