6e2e99b5078bf448999f608ff265c76c84b144a1
filmee24 <filmee24@gmail.com>
2022-07-06 17:07:57 +0200

    Add Method To Check If Cursor Is In Nodes Range

---
+++ LSP/SourceRangeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Loyc.Syntax;
+
+namespace LSP_Server
+{
+    public static class SourceRangeExtensions
+    {
+        public static bool Contains(this SourceRange range, int line, int column)
+        {
+            var inLeft = false;
+            if (range.Start.Line < line) inLeft = true;
+            else if (range.Start.Line > line) inLeft = false;
+            else inLeft = range.Start.Column <= column;
+
+            var inRight = false;
+            if (range.Start.Line > line) inRight = true;
+            else if (range.Start.Line < line) inRight = false;
+            else inRight = range.Start.Column >= column;
+
+            return inLeft && inRight;
+        }
+    }
+}
