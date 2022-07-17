dd5d03241423d5b334c5e9b9b7dda610956b3948
filmee24 <filmee24@gmail.com>
2022-07-17 09:47:47 +0200

    Add Some Keywords To Completion

--- LSP/CompletionScopes/RootCompletionScope.cs
+++ LSP/CompletionScopes/RootCompletionScope.cs
@@ -13,6 +13,10 @@ namespace LSP_Server
             yield return new CompletionItem() { Label = "using", Kind = CompletionItemKind.Keyword };
             yield return new CompletionItem() { Label = "implement", Kind = CompletionItemKind.Keyword };
             yield return new CompletionItem() { Label = "func", Kind = CompletionItemKind.Keyword };
+            yield return new CompletionItem() { Label = "type", Kind = CompletionItemKind.Keyword };
+            yield return new CompletionItem() { Label = "union", Kind = CompletionItemKind.Keyword };
+            yield return new CompletionItem() { Label = "class", Kind = CompletionItemKind.Keyword };
+            yield return new CompletionItem() { Label = "struct", Kind = CompletionItemKind.Keyword };
         }
     }
 }
