b3ea400be52c715b559849c0615ab327de031954
filmee24 <filmee24@gmail.com>
2022-08-27 12:11:49 +0200

    Cleanup

--- LSP/Handlers/HoverHandler.cs
+++ LSP/Handlers/HoverHandler.cs
@@ -5,70 +5,69 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 
-namespace LSP_Server.Handlers
+namespace LSP_Server.Handlers;
+
+public class HoverHandler : IHoverHandler
 {
-    public class HoverHandler : IHoverHandler
+    private readonly ILanguageServerFacade protocolProxy;
+    private readonly BufferManager _bufferManager;
+
+    public HoverHandler(BufferManager bufferManager, ILanguageServerFacade protocolProxy)
     {
-        private readonly ILanguageServerFacade protocolProxy;
-        private readonly BufferManager _bufferManager;
+        _bufferManager = bufferManager;
+        this.protocolProxy = protocolProxy;
+    }
 
-        public HoverHandler(BufferManager bufferManager, ILanguageServerFacade protocolProxy)
+    public HoverRegistrationOptions GetRegistrationOptions(HoverCapability capability, ClientCapabilities clientCapabilities)
+    {
+        return new HoverRegistrationOptions
         {
-            _bufferManager = bufferManager;
-            this.protocolProxy = protocolProxy;
+            DocumentSelector = TextDocumentSyncHandler.DocumentSelector,
+            WorkDoneProgress = false
+        };
+    }
+
+    public Task<Hover?> Handle(HoverParams request, CancellationToken token)
+    {
+        var documentPath = request.TextDocument.Uri.ToString();
+        var buffer = _bufferManager.GetBuffer(documentPath);
+
+        LNode matchingNode = LNode.Missing;
+
+        foreach (var node in buffer.Descendants())
+        {
+            if (node.Range.Length == 0 || node == null) continue;
+
+            if (node.Range.Contains(request.Position.Line + 1, request.Position.Character + 1))
+            {
+                matchingNode = node;
+            }
         }
 
-        public HoverRegistrationOptions GetRegistrationOptions(HoverCapability capability, ClientCapabilities clientCapabilities)
+        if (matchingNode != LNode.Missing)
         {
-            return new HoverRegistrationOptions
+            string content = "";
+
+            if (matchingNode.ArgCount == 1 && matchingNode.Args[0].HasValue)
             {
-                DocumentSelector = TextDocumentSyncHandler.DocumentSelector,
-                WorkDoneProgress = false
-            };
-        }
-
-        public Task<Hover?> Handle(HoverParams request, CancellationToken token)
-        {
-            var documentPath = request.TextDocument.Uri.ToString();
-            var buffer = _bufferManager.GetBuffer(documentPath);
-
-            LNode matchingNode = LNode.Missing;
-
-            foreach (var node in buffer.Descendants())
+                content = matchingNode.Name.Name;
+            }
+            else
             {
-                if (node.Range.Length == 0 || node == null) continue;
-
-                if (node.Range.Contains(request.Position.Line + 1, request.Position.Character + 1))
-                {
-                    matchingNode = node;
-                }
+                content = matchingNode.ToString();
             }
 
-            if (matchingNode != LNode.Missing)
+            return Task.FromResult<Hover?>(new Hover()
             {
-                string content = "";
-
-                if (matchingNode.ArgCount == 1 && matchingNode.Args[0].HasValue)
-                {
-                    content = matchingNode.Name.Name;
-                }
-                else
-                {
-                    content = matchingNode.ToString();
-                }
-
-                return Task.FromResult<Hover?>(new Hover()
-                {
-                    Contents =
-                        new MarkedStringsOrMarkupContent(new MarkupContent()
-                        {
-                            Value = matchingNode.Name.Name + ": " + content,
-                            Kind = MarkupKind.PlainText
-                        })
-                });
-            }
-
-            return Task.FromResult<Hover?>(null);
+                Contents =
+                    new MarkedStringsOrMarkupContent(new MarkupContent()
+                    {
+                        Value = matchingNode.Name.Name + ": " + content,
+                        Kind = MarkupKind.PlainText
+                    })
+            });
         }
+
+        return Task.FromResult<Hover?>(null);
     }
 }
