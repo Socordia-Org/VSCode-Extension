34e8c5f05ed512e45ecc3e7f092b957dae26473d
filmee24 <filmee24@gmail.com>
2022-08-27 12:11:33 +0200

    Add Keywords

--- LSP/CompletionScopes/RootCompletionScope.cs
+++ LSP/CompletionScopes/RootCompletionScope.cs
@@ -12,12 +12,14 @@ public class RootCompletionScope : ContextCompletionHandler
     public override IEnumerable<CompletionItem> GetItems(LNode node)
     {
         yield return new CompletionItem() { Label = "using", Kind = CompletionItemKind.Keyword };
+        yield return new CompletionItem() { Label = "unit", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "implement", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "func", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "type", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "union", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "class", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "struct", Kind = CompletionItemKind.Keyword };
+        yield return new CompletionItem() { Label = "bitfield", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "module", Kind = CompletionItemKind.Keyword };
     }
 }
