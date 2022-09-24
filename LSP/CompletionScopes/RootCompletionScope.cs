2f46d0069c31f6b048309587cbceac10ac5f9ba8
filmee24 <filmee24@gmail.com>
2022-09-24 20:42:05 +0200

    Add Keywords To RootScope

--- LSP/CompletionScopes/RootCompletionScope.cs
+++ LSP/CompletionScopes/RootCompletionScope.cs
@@ -18,8 +18,10 @@ public class RootCompletionScope : ContextCompletionHandler
         yield return new CompletionItem() { Label = "func", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "type", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "union", Kind = CompletionItemKind.Keyword };
+        yield return new CompletionItem() { Label = "unit", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "class", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "struct", Kind = CompletionItemKind.Keyword };
+        yield return new CompletionItem() { Label = "interface", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "bitfield", Kind = CompletionItemKind.Keyword };
         yield return new CompletionItem() { Label = "module", Kind = CompletionItemKind.Keyword };
     }
