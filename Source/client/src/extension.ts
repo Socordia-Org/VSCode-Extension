a7a2b5d6c7fa3c268a0bc71fd23229366a33de50
Chris <filmee24@gmail.com>
2022-10-15 20:19:20 +0200

    Update Packages

--- Source/client/src/extension.ts
+++ Source/client/src/extension.ts
@@ -1,42 +1,58 @@
-'use strict';
+"use strict";
 
-import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import * as path from "path";
+import { PlatformInformation } from "./platform";
+import * as coreclrdebug from "./coreclr-debug/activate";
+import { workspace, ExtensionContext } from "vscode";
 import {
   LanguageClient,
   LanguageClientOptions,
   ServerOptions,
-  TransportKind
-} from 'vscode-languageclient/node';
+  TransportKind,
+} from "vscode-languageclient/node";
 
-export function activate(context: ExtensionContext) {
+export async function activate(context: ExtensionContext) {
+  let platformInfo: PlatformInformation;
+  try {
+    platformInfo = await PlatformInformation.GetCurrent();
+  } catch (error) {
+    throw error;
+  }
 
-    let serverExe = "dotnet";
+  let serverExe = "dotnet";
 
-    let server = context.asAbsolutePath(
-      path.join("bin", "LSP-Server.dll")
-    )
+  let server = context.asAbsolutePath(path.join("bin", "LSP-Server.dll"));
 
-    let serverOptions: ServerOptions = {
-        run: { command: serverExe, args: [server] },
-        debug: { command: serverExe, args: [server] }
-    }
+  let serverOptions: ServerOptions = {
+    run: { command: serverExe, args: [server] },
+    debug: { command: serverExe, args: [server] },
+  };
 
-    // Options to control the language client
-    let clientOptions: LanguageClientOptions = {
-        documentSelector: [
-            {
-                pattern: '**/*.back',
-            }
-        ],
-        
-        synchronize: {
-            configurationSection: 'BacklangLanguageServer',
-            fileEvents: workspace.createFileSystemWatcher('**/*.back')
-        },
-    }
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [
+      {
+        pattern: "**/*.back",
+      },
+    ],
 
-    const client = new LanguageClient('BacklangLanguageServer', 'Backlang Language Server', serverOptions, clientOptions);
-    
-    client.start();
+    synchronize: {
+      configurationSection: "BacklangLanguageServer",
+      fileEvents: workspace.createFileSystemWatcher("**/*.back"),
+    },
+  };
+
+  let coreClrDebugPromise = Promise.resolve();
+
+  // activate coreclr-debug
+  coreClrDebugPromise = coreclrdebug.activate(context, platformInfo);
+
+  const client = new LanguageClient(
+    "BacklangLanguageServer",
+    "Backlang Language Server",
+    serverOptions,
+    clientOptions
+  );
+
+  client.start();
 }
